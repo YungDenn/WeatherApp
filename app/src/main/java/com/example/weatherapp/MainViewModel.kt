e3364f57a754e8ee500ef5d24f2b1538a9a4bd75
YungDenn <denn.033@mail.ru>
2022-10-20 17:02:16 +0500

    Crete CardView for rv weather

--- app/src/main/java/com/example/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/MainViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.weatherapp
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.weatherapp.adpters.WeatherModel
 
 class MainViewModel: ViewModel() {
-
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<WeatherModel>()
 }
